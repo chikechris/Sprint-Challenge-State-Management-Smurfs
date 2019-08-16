6ba27b9abfa579fa5cdb3c1e5af2058aae8bc56e
Chike Chris <cchike2017@gmail.com>
2019-08-16 12:53:22 -0400

    built out the  initialState in reducer file

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -4,19 +4,30 @@ import {
   FETCH_SMURFS_FAILURE
 } from "../actions";
 
+const inititalState = {
+  smurfs: [],
+  isLoading: false,
+  error: ""
+};
+
 const reducer = (state = inititalState, action) => {
   switch (action.type) {
     case FETCH_SMURFS_START:
       return {
-        ...state
+        ...state,
+        isLoading: true
       };
     case FETCH_SMURFS_SUCCESS:
       return {
-        ...state
+        ...state,
+        isLoading: false,
+        smurfs: action.payload
       };
     case FETCH_SMURFS_FAILURE:
       return {
-        ...state
+        ...state,
+        isLoading: false,
+        error: "Cant load smurfs!!!!"
       };
     default:
       return state;
