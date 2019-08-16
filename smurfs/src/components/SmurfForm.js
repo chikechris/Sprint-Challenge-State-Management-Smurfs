c700bc5c9cf2778f21e7f90d89f5ae32fb14a45c
Chike Chris <cchike2017@gmail.com>
2019-08-16 12:13:23 -0400

    created a mock function, imported them in app.js for rendering

---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SmurfForm = () => {
+  return (
+    <div>
+      <h2>Smurf</h2>
+    </div>
+  );
+};
+
+export default SmurfForm;
---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SmurfList = () => {
+  return (
+    <div>
+      <h3>SmurfList</h3>
+    </div>
+  );
+};
+
+export default SmurfList;
